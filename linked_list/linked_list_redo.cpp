be9808e2fa1207dd518538d4171c450c0c6af40e
manujagobind <manujagobind@gmail.com>
2017-04-02 22:36:33 +0530

    Continue with Linked List

--- linked_list/linked_list_redo.cpp
+++ linked_list/linked_list_redo.cpp
@@ -30,6 +30,12 @@ public:
 	T pop_front();
 	void push_back(T data);
 	T pop_back();
+	int size();
+	void reverse();
+	T value_at(int);
+	void insert_at(int, T);
+	void erase_at(int);
+	T value_from_end(int);	
 };
 
 template <typename T>
@@ -132,6 +138,88 @@ void LinkedList<T>::push_back(T data){
 	}	
 }
 
+template <typename T>
+int LinkedList<T>::size(){
+	
+	int count = 0;
+	if(Node<T> *cur = this->get_head()){
+		
+		while(cur){
+			count++;
+			cur = cur->get_next();
+		}
+	}
+	return count;
+}
+
+template <typename T>
+void LinkedList<T>::reverse(){
+	
+	Node<T> *a;
+	//Check if list is not empty
+	if(a = this->get_head()){					
+		if(a->has_next()){
+			//List should have at least 2 elements so that we can reverse
+			//(i.e. No need to reverse if only 1 element)
+			Node<T> *b, *c;
+			b = a->get_next();			
+			a->set_next(NULL);
+			while(b->has_next()){
+				c = b->get_next();
+				b->set_next(a);
+				a = b;
+				b = c;
+			}
+			b->set_next(a);
+			this->set_head(b);
+		} 
+	}
+}
+
+template <typename T>
+T LinkedList<T>::value_at(int index){	
+	//Indexing starts at 0
+	int count = 0;
+	Node<T> *cur = this->get_head();
+	
+	while(cur && count < index){		
+		cur = cur->get_next();
+		count++;
+	}
+	
+	if(count == index){
+		return cur->get_data();
+	} else{
+		cout << "\nInvalid Index\n";
+		return ERROR;
+	}
+}
+
+template <typename T>
+void LinkedList<T>::insert_at(int index, T data){
+	//Indexing starts at 0
+	int count = 0;	
+	Node<T> *cur = this->get_head();
+	Node<T> *prev = NULL;
+	Node<T> *node = new Node<T>(data);
+	
+	while(cur && count < index){
+		prev = cur;
+		cur = cur->get_next();
+		count++;
+	}
+	
+	if(count == 0){
+		node->set_next(cur);
+		this->set_head(node);
+	} else if(count == index){		
+		node->set_next(cur);
+		prev->set_next(node);
+	} else{
+		cout << "\nInvalid Index\n";
+	}
+}
+
 int main(){
 	
 	LinkedList<int> L;
@@ -139,7 +227,12 @@ int main(){
 	L.push_back(5);
 	L.push_front(10);
 	L.push_front(3);
-	L.push_back(5);	
-	L.print();
+	L.push_back(6);	
+	L.print();	
+	cout << L.value_at(3) << endl;
+	L.insert_at(3, 7);	
+	//L.reverse();
+	L.print();	
+	cout << L.value_at(6) << endl;
 	return 0;
 }
